d28f96d486b3b0af3b4137e451d16c4e83c11760
Alex Kozar <alexkozar1306@gmail.com>
2019-09-16 02:16:57 +0300

    Updated calculation constant

--- src/app/shared/constants/prices.constant.ts
+++ src/app/shared/constants/prices.constant.ts
@@ -11,25 +11,25 @@ export const PRICES = {
     delivery: 450
   },
   bergen : {
-    kitchen: 10748,
-    down: -3985,
-    mezzanine: 1395,
-    island: 6763,
+    kitchen: 11706,
+    down: -3963,
+    mezzanine: 2404,
+    island: 7743,
     freezer: 1209,
     oven: 1395,
-    shelves: 1495,
-    mounting: 2351,
+    shelves: 1432,
+    mounting: 1121,
     delivery: 450
   },
   solo : {
-    kitchen: 10748,
-    down: -3985,
-    mezzanine: 1395,
-    island: 6763,
-    freezer: 1209,
-    oven: 1395,
-    shelves: 1495,
-    mounting: 2351,
+    kitchen: 8955,
+    down: -3306,
+    mezzanine: 2404,
+    island: 5649,
+    freezer: 1005,
+    oven: 1105,
+    shelves: 1332,
+    mounting: 1121,
     delivery: 450
   }
 };
