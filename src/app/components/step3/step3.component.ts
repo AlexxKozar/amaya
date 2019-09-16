507650208e895ffd4e33f8379e054038015a34ec
Alex Kozar <alexkozar1306@gmail.com>
2019-09-17 00:36:06 +0300

    Add validation to step3

--- src/app/components/step3/step3.component.ts
+++ src/app/components/step3/step3.component.ts
@@ -3,6 +3,8 @@ import { CalculationService } from '@services/calculation.service';
 import { Router } from '@angular/router';
 import { SCHEMA_IMAGE_FILENAMES } from '@constants/image-filenames.constant';
 import { SCHEMA_INPUTS } from '@constants/schema-inputs.constant';
+import {FormControl, FormGroup, Validators} from '@angular/forms';
+
 @Component({
   selector: 'app-step3',
   templateUrl: './step3.component.html',
@@ -10,17 +12,20 @@ import { SCHEMA_INPUTS } from '@constants/schema-inputs.constant';
 })
 export class Step3Component implements OnInit {
 
-  public sizeModel = {
-    l1: 0,
-    l2: 0,
-    l3: 0,
-  };
-  public imageName: string;
+  public sizeForm = new FormGroup({
+    top: new FormControl(),
+    left: new FormControl(),
+    right: new FormControl()
+  });
+
   public inputsDisplay = {
     top: false,
     left: false,
     right: false
   };
+
+  public imageName: string;
+  public formInvalid = false;
 
   constructor(
     private router: Router,
@@ -29,16 +34,21 @@ export class Step3Component implements OnInit {
 
   ngOnInit() {
     this._setInitialData();
+    this._initFormControlsByObjectKeys(this.sizeForm, this.inputsDisplay);
   }
 
   public onFormSubmit() {
-    const sizes = [
-      this.sizeModel.l1,
-      this.sizeModel.l2,
-      this.sizeModel.l3
-    ];
+    const sizes = [];
+    for (const size of this.sizeForm.value) {
+      if (size) { sizes.push(size); }
+    }
     this.calculationService.setSizes(sizes);
-    this.router.navigate(['/step4']);
+
+    if (this.sizeForm.valid) {
+        this.router.navigate(['/step4']);
+    } else {
+      this.formInvalid = true;
+    }
   }
 
   private _setInitialData() {
@@ -46,6 +56,18 @@ export class Step3Component implements OnInit {
     const { schema } = formsData;
     this.imageName = SCHEMA_IMAGE_FILENAMES[schema];
     this.inputsDisplay = SCHEMA_INPUTS[schema];
+  }
+
+  private _initFormControlsByObjectKeys(form: FormGroup, object: any) {
+    for (const key in object) {
+      if (object[key]) {
+        form.setControl(key, new FormControl(null,  [
+          Validators.required,
+          Validators.pattern('^[0-9]*$'),
+          Validators.minLength(2)
+        ]));
+      }
+    }
   }
 
 }
