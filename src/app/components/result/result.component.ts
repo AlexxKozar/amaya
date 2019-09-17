4bb0e17c16de0c4be68363b030995a77361f95ee
Alex Kozar <alexkozar1306@gmail.com>
2019-09-17 21:11:49 +0300

    Add thanks page && updated layout

--- src/app/components/result/result.component.ts
+++ src/app/components/result/result.component.ts
@@ -3,6 +3,7 @@ import { Router } from '@angular/router';
 import { CalculationService } from '@services/calculation.service';
 import { CalculationDataInterface } from '@interfaces/calculation-data.interface';
 import { STYLE_IMAGE_FILENAMES } from '@constants/image-filenames.constant'
+import { ValueFormattersHelpers } from '@helpers/value-formatters.helpers';
 
 @Component({
   selector: 'app-result',
@@ -38,17 +39,26 @@ export class ResultComponent implements OnInit {
   ) { }
 
   ngOnInit() {
+    this._formatDataToMonetFormat(this.calculationData);
     this._setInitialData();
   }
 
   onFormSubmit() {
+    this.router.navigate(['/thanks']);
   }
 
   private _setInitialData() {
     this.calculationData = this.calculationService.calculateKitchenPrice();
+    this._formatDataToMonetFormat(this.calculationData);
     this.formsData = this.calculationService.getFormsData();
     this.kitchenImageFilename = STYLE_IMAGE_FILENAMES[this.formsData.style];
     console.log(this.calculationData);
+  }
+
+  private _formatDataToMonetFormat(data) {
+    for (const key in data) {
+      data[key] = ValueFormattersHelpers.toMoneyFormat(data[key]) + ' грн';
+    }
   }
 
 }
