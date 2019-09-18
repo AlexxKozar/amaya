2656955ee92e1898534da57b85efe8378cee8674
illindima <illindima@gmail.com>
2019-09-18 22:55:27 +0300

    delete duplicate

--- index.php
+++ index.php
@@ -33,7 +33,6 @@ $app->addErrorMiddleware(true, false, false);
 $app->add(function($request, $handler) {
     $response = $handler->handle($request);
     $response = $response
-        ->withHeader('Access-Control-Allow-Methods', '*')
         ->withHeader('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Accept, Origin, Authorization')
         ->withHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
     
